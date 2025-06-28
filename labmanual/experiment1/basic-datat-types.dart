d427997f82c026e81294ea8737cafb69bf20e6ff
MS <72651078+maddydevgits@users.noreply.github.com>
2025-06-28 20:06:56 +0530

    Create basic-datat-types.dart

---
+++ labmanual/experiment1/basic-datat-types.dart
@@ -0,0 +1,8 @@
+void main() {
+  int age = 25;
+  double height = 5.9;
+  bool isStudent = true;
+  String name = "Sravani";
+
+  print("Name: $name, Age: $age, Height: $height, Student: $isStudent");
+}
